a789b7643527554c337de12ced44678aeb7b3268
nisfu-saaban <nisthy07@gmail.com>
2023-05-26 21:07:50 +0700

    mutex for race condition

--- race_condition_test.go
+++ race_condition_test.go
@@ -2,6 +2,7 @@ package golanggoroutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -13,6 +14,23 @@ func TestRaceConditio(t *testing.T) {
 		go func() {
 			for j := 1; j <= 100; j++ {
 				x = x + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Jumlah X ", x)
+}
+
+func TestMutexRaceConditio(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
 			}
 		}()
 	}
